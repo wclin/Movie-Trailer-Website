a44b08c7ef8d7a445d0c1b93feba2729ecd4a35e
wclin <r03922092+git@csie.ntu.edu.tw>
2016-06-16 01:02:01 +0800

    I don't know what I am drawing.

--- MovieTrailer/DrawingTurtle/mindstorms.py
+++ MovieTrailer/DrawingTurtle/mindstorms.py
@@ -1,18 +1,20 @@
 import turtle
 
-def draw_square():
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("green")
-
-    brad = turtle.Turtle()
-    brad.shape("turtle")
-    brad.color("yellow")
-    brad.speed(3)
-    step = 10
-    while step < 500:
-        brad.forward(step)
-        brad.right(90)
-        step *= 1.2
+    
+    t = turtle.Turtle()
+    t.speed(20)
+    step = 200
+    while step > 1:
+        t.forward(step)
+        t.right(179)
+        step -= 1
+        t.forward(step)
+        t.left(179)
+        step -= 1
     window.exitonclick()
 
-draw_square()
+draw_art()
+
