fa28151915a03de6a386732e1b26fd1876a567ee
wclin <r03922092+git@csie.ntu.edu.tw>
2016-06-15 19:04:22 +0800

    feat: break 3 times.

--- MovieTrailer/breaktime.py
+++ MovieTrailer/breaktime.py
@@ -1,5 +1,11 @@
 import time
 import webbrowser as wb
 
-time.sleep(10)
-wb.open("https://www.youtube.com/watch?v=GaoLU6zKaws")
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while(break_count < total_breaks):
+  time.sleep(3)
+  wb.open("https://www.youtube.com/watch?v=GaoLU6zKaws")
+  break_count += 1
