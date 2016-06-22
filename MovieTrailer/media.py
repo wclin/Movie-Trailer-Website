67e747db32eafbf4bdbd3705ed7f9cf18cc0748f
wclin <r03922092+git@csie.ntu.edu.tw>
2016-06-23 06:46:22 +0800

    Link titles, posters, trailers.

--- MovieTrailer/media.py
+++ MovieTrailer/media.py
@@ -2,4 +2,7 @@ import webbrowser
 
 class Movie():
     """A simple movie datastructure"""
-    def __init__(self, movie_titles, movie)
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
--- MovieTrailer/setup.py
+++ MovieTrailer/setup.py
@@ -1,3 +1,8 @@
 import fresh_potatoes as fp
+import media
 
-fp.open_movies_page()
+mv = media.Movie('Showtimes for A Hologram for the King',
+        'http://t3.gstatic.com/images?q=tbn:ANd9GcTn4cbbsC68ZyzFYf70wQKBI7ftcPFr26u2kF11ZDaI00CeuAo9',
+        'https://www.youtube.com/watch?v=eJtVctN0xIM')
+
+fp.open_movies_page({mv})
