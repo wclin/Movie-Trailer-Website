8c46a929cae16866121e367a496ebf3889bd8e7f
wclin <r03922092+git@csie.ntu.edu.tw>
2016-06-23 15:44:36 +0800

    Ignore pyc and html.

--- MovieTrailer/setup.py
+++ MovieTrailer/setup.py
@@ -7,8 +7,5 @@ mv_list = []
 
 for tmdb_id in pop_list:
     mv_list.append(media.Movie(tmdb_id))
-#mv = media.Movie('Showtimes for A Hologram for the King',
-#        'http://t3.gstatic.com/images?q=tbn:ANd9GcTn4cbbsC68ZyzFYf70wQKBI7ftcPFr26u2kF11ZDaI00CeuAo9',
-#        'https://www.youtube.com/watch?v=eJtVctN0xIM')
 
 fp.open_movies_page(mv_list)
